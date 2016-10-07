68f3968344d9fff026ae251746b68eb3804a6474
Hitender Razora <hitenderr.azora@gmail.com>
2016-10-07 07:43:56 +0100

    Hitender: Added a new project to do small spikes, code snippets and Research and Development

---
+++ rnd/src/main/java/com/vlearn/App.java
@@ -0,0 +1,13 @@
+package com.vlearn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
